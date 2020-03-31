afe42e11f6b6ec4728affe8200c46a9b0187315a
Adetunji <Ashennaike@gmail.com>
2020-03-31 15:48:18 -0400

    child block for parentla portal done, simple version

---
+++ activity-tracker-fe/src/components/ChildBlock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const ChildBlock = ({ child }) => {
+  
+  return(
+    <div>
+      <p>Child Name{/*child.name*/}</p>
+      <p>Activity Points{/*child.activityPoints*/}</p>
+    </div>
+  )
+}
