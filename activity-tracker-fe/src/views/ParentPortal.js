da219d858393f58a19e874267e9a1bac282fe838
Adetunji <Ashennaike@gmail.com>
2020-03-31 18:33:37 -0400

    waiting for backend points for axios calls will work on next ocmponent after styling

--- activity-tracker-fe/src/views/ParentPortal.js
+++ activity-tracker-fe/src/views/ParentPortal.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ChildBlock from '../components/ChildBlock'
 
 const ParentPortal = () => {
-  const 
+  const [children, setChildren] = useState([])
+
+  useEffect(() => {
+    axios.get("user's children")
+    .then(res => {
+      setChildren(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
   
   return (
+    <div>
+      {children.forEach(kid => {
+        <ChildBlock child={kid} />
+      })}
 
+    </div>
   )
 }
 
